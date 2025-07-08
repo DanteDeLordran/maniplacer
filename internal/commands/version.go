d7538d0798f3e9a2dbd74cd43efcdb396820c94c
lordran <lordran@darsaras.com>
2025-07-07 23:51:04 -0600

    Separation of concerns

---
+++ internal/commands/version.go
@@ -0,0 +1,13 @@
+package commands
+
+import "fmt"
+
+const VERSION = "1.0.0"
+
+func Help() {
+	fmt.Println("Usage: maniplacer new -f <path to json>")
+}
+
+func Version() {
+	fmt.Println(VERSION)
+}
