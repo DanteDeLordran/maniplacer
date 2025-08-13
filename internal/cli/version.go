262bda8562cc2319c85fae895cb9e32bd74a7fc1
lordran <lordran@darsaras.com>
2025-08-12 21:21:38 -0600

    Print logo on version

--- internal/cli/version.go
+++ internal/cli/version.go
@@ -11,12 +11,21 @@ import (
 
 const version = "1.0.0"
 
+const logo = `
+                       _       _                     
+ _ __ ___   __ _ _ __ (_)_ __ | | __ _  ___ ___ _ __ 
+| '_ ' _ \ / _' | '_ \| | '_ \| |/ _' |/ __/ _ \ '__|
+| | | | | | (_| | | | | | |_) | | (_| | (_|  __/ |   
+|_| |_| |_|\__,_|_| |_|_| .__/|_|\__,_|\___\___|_|   
+                        |_|                                                         
+`
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Returns the current version of Maniplacer",
 	Long:  `Returns the current version of Maniplacer`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Version: %s\n", getVersion())
+		fmt.Printf("%s\nVersion: %s\n", logo, getVersion())
 	},
 }
 
