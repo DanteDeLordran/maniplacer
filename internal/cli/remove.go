63c7a67a094762213df2ab3a52c12d0413176b76
lordran <lordran@darsaras.com>
2025-08-15 18:55:32 -0600

    Remove command

---
+++ internal/cli/remove.go
@@ -0,0 +1,77 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/dantedelordran/maniplacer/internal/utils"
+	"github.com/spf13/cobra"
+)
+
+var removeCmd = &cobra.Command{
+	Use:   "remove",
+	Short: "Removes a component from the templates dir given a namespace, defaults to default namespace",
+	Long: `Removes a component from the templates dir given a namespace, if no namespace is provided, it will search for it in default namespace
+
+Available removable components:
+- Service
+- Deployment
+- HttpRoute
+- Secret
+- ConfigMap`,
+	Args: cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+
+		if !utils.IsValidProject() {
+			fmt.Printf("Current directory is not a valid Maniplacer project\n")
+			os.Exit(1)
+		}
+
+		namespace, err := cmd.Flags().GetString("namespace")
+		if err != nil {
+			fmt.Printf("Could not parse namespace flag due to %s, using default", err)
+			namespace = "default"
+		}
+
+		currentDir, err := os.Getwd()
+		if err != nil {
+			fmt.Printf("Could not get current directory due to %s", err)
+			os.Exit(1)
+		}
+
+		namespacePath := filepath.Join(currentDir, "templates", namespace)
+
+		_, err = os.Stat(namespacePath)
+		if err != nil {
+			fmt.Printf("No namespace with name %s\n", namespace)
+			os.Exit(1)
+		}
+
+		for _, comp := range args {
+			templatePath := filepath.Join(currentDir, "templates", namespace, fmt.Sprintf("%s.yaml", comp))
+
+			file, err := os.Stat(templatePath)
+			if err != nil {
+				fmt.Printf("Component %s does not exist in templates dir with %s namespace, skipping...\n", comp, namespace)
+			} else {
+				fmt.Printf("Removing %s...\n", file.Name())
+
+				err = os.Remove(templatePath)
+				if err != nil {
+					fmt.Printf("Could not remove file due to %s\n", err)
+					continue
+				}
+
+				fmt.Printf("Successfully removed %s from %s namespace\n", file.Name(), namespace)
+			}
+
+		}
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(removeCmd)
+	removeCmd.Flags().StringP("namespace", "n", "default", "Namespace for removing templates")
+}
--- roadmap.md
+++ roadmap.md
@@ -8,8 +8,8 @@
 
 - [x] Version: Shows current version
 - [x] Init: Inits a new project given a name and optional config type flag (default JSON)
-- [ ] Add: Adds a new empty component template
-- [ ] Remove: Removes an empty component template by namespace
+- [x] Add: Adds a new empty component template
+- [x] Remove: Removes a component template by namespace
 - [ ] Generate: Generates a new manifest
 - [ ] List: Lists all the manifests in order filtered by namespace
 - [ ] Update: Self update to latest version
